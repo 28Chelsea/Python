ef3e6985a85cd2d63997c1ba912d397ba8e682c0
28Chelsea <28Chelsea@gmail.com>
2020-08-30 16:23:42 -0500

    Initial commit

---
+++ myfirstcode.py
@@ -0,0 +1,12 @@
+# print("Chelsea")
+# print("Happy Birthday to you,")
+# print("Happy Birthday to you,")
+
+def singHappyBirthday(name):
+    for var in list(range(2)):
+        print(("Happy Birthday to you"))
+
+    print("Happy Birthday dear, " + name)
+    print("Happy Birthday to you!")
+
+singHappyBirthday("Claudius")
